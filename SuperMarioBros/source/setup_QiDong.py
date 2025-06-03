c6f88089b5ae5a452dda2acd6dc5b28344b2da77
ShiWenyan <2053861201@qq.com>
2025-06-03 18:40:07 +0800

    Delete SuperMarioBros directory

--- SuperMarioBros/source/setup_QiDong.py
+++
@@ -1,8 +0,0 @@
-# 存放  游戏启动时 的代码
-import pygame
-from. import constants_ChangLiang as C
-from. import tools
-# 初始化屏幕
-pygame.init()
-SCREEN = pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))#这里的宽高用常量表示
-GRAPHICS = tools.load_graphics('resources/graphics')
